bdbd810fcb716405521913d72cd3caa7764ba83b
Rajat <rajatkantinandi@github.com>
2018-06-05 22:39:36 +0530

    Added insertFirst & choice

--- Week 4/Data structures/linked_list.c
+++ Week 4/Data structures/linked_list.c
@@ -11,20 +11,32 @@ typedef struct
   struct node *next;
 } node;
 node *insertLast(char *value, node *last);
+node *insertFirst(char *value,node *head);
 void printList(node *list);
 void freeMemory(node *list);
 int main(int argc, char const *argv[])
 {
   node *head = NULL;
   node *tail = NULL;
+  int choice=1;
+  printf("Enter the mode of insertion: \n 1) At First\n 2) At Last \n");
+  scanf("%d",&choice);
   if (argc > 0)
   {
     for (int i = 1; i < argc; i++)
     {
-      tail = insertLast(argv[i], tail);
-      if (head == NULL)
-      {
-        head = tail;
+      if(choice==1){
+        head=insertFirst(argv[i],head);
+      }
+      else if(choice==2){
+        tail = insertLast(argv[i], tail);
+        if (head == NULL)
+        {
+          head = tail;
+        }
+      }
+      else{
+        printf("Invalid choice !! \n");
       }
     }
     printf("The list: \n");
@@ -57,6 +69,26 @@ node *insertLast(char *value, node *last)
     else
       last->next = newItem;
     return newItem;
+  }
+}
+node *insertFirst(char *value, node *head)
+{
+  node *newItem = malloc(sizeof(node));
+  if (newItem == NULL)
+  {
+    return NULL;
+  }
+  else
+  {
+    newItem->value = value;
+    if (head == NULL)
+    {
+      newItem->next = NULL;
+    }
+    else
+      newItem->next = head;
+    head=newItem;
+    return head;
   }
 }
 void printList(node *list)
